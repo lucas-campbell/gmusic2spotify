8891f4f50e118615cc4178e2e6db7fcf774f1a51
Lucas Campbell <lucas.campbell197@gmail.com>
2019-02-09 22:51:26 +0100

    update .gitignore, possibly comments as well

--- playlist.py
+++ playlist.py
@@ -1,14 +1,15 @@
-# playlist_type.py -- defines class of playlist type
+# playlist_type.py -- defines class of playlist type.
+# Has a title and list of track objects (see tracks.py)
 
 class playlist_type:
-    def __init__(self):
-        self.title=''
-        self.numTracks=0
-        self.tracks={}
+    def __init__(self, x='', y={}):
+        self.title  = x
+        self.tracks = y
 
     def readFromGMusic(self, gmusicPL):
         self.title = gmusicPL['name']
         for t in gmusicPL['tracks']:
+            #some sort of check for if track is hosted on gmusic
             if (t['source'] == 
             self.tracks[t
 
