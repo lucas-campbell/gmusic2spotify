42546970fefb3255315f6193d250684629fcef1a
Lucas Campbell <lucas.campbell197@gmail.com>
2020-04-29 11:59:30 -0400

    comments in flask_server

--- flask_server.py
+++ flask_server.py
@@ -1,7 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-#NOTE: need to `export FLASK_APP=flask_server.py`
+# NOTE: need to `export FLASK_APP=flask_server.py`
+# and be sure that flask is installed on the system
+#   (sudo apt install python3-flask)
+# then, inside VENV run `flask run`
 
 @app.route('/')
 def hello_world():
---
+++ setup.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+export FLASK_APP="flask_server.py"
